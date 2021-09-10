80430bf0810cd66ce66ffdc815e4a5ae07d06320
Ashwindaniel <53498110+Ad025@users.noreply.github.com>
2021-09-10 14:52:57 +1000

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,121 @@
+import { Redirect, Route } from 'react-router-dom';
+import {
+  IonApp,
+  IonFooter,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { ellipse, square, triangle } from 'ionicons/icons';
+import Tab1 from './pages/Tab1';
+import Tab2 from './pages/Tab2';
+import Tab3 from './pages/Tab3';
+import Tab4 from './pages/Tab4';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       ho
+//     </div>
+//   );
+// }
+
+const App: React.FC = () => (
+  <IonApp>
+
+    
+    <IonReactRouter>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Route exact path="/tab1">
+            <Tab1 />
+          </Route>
+          <Route exact path="/tab2">
+            <Tab2 />
+          </Route>
+          <Route path="/tab3">
+            <Tab3 />
+          </Route>
+          <Route path="/tab4">
+            <Tab4 />
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/tab1" />
+          </Route>
+        </IonRouterOutlet>
+
+        
+
+        <IonTabBar slot="top">
+          <IonTabButton tab="tab1" href="/tab1">
+            <IonTabButton >Home</IonTabButton>
+          </IonTabButton>
+          <IonTabButton tab="tab2" href="/tab2">
+            <IonTabButton>About</IonTabButton>
+          </IonTabButton>
+          <IonTabButton tab="tab3" href="/tab3">
+            <IonTabButton>Map</IonTabButton>
+          </IonTabButton>
+          <IonTabButton tab="tab4" href="/tab4" >
+            <IonTabButton>News</IonTabButton>
+          </IonTabButton>
+          
+
+
+          
+
+        </IonTabBar>
+      
+        {/* <IonFooter slot="bottom">
+          <IonToolbar slot='top' color="dark">
+          <IonTitle>Dark Toolbar</IonTitle>
+          </IonToolbar>
+        </IonFooter> */} 
+
+  
+
+        {/* <IonFooter slot="bottom">
+          <IonToolbar>
+            <IonTitle>Footer</IonTitle>
+          </IonToolbar>
+        </IonFooter> */}
+
+      </IonTabs>
+
+     
+
+      
+    </IonReactRouter>
+
+      
+  </IonApp>
+
+  
+);
+
+export default App;
